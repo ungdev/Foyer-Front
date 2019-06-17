4b4ac7695f688f8de9a34510f4285481322d88b4
Arnaud9145 <arnaud.dufour@utt.fr>
2019-06-17 10:08:22 +0200

    added role in asso

--- src/containers/dashboard/pages/AssoManagement.jsx
+++ src/containers/dashboard/pages/AssoManagement.jsx
@@ -6,67 +6,86 @@ import './styles/asso.css'
 import DiapoImage from './components/DiapoImage'
 
 class AssoManagement extends React.Component {
-
   tabList = [
     {
       key: 'presentation',
-      tab: 'Présentation de l\'asso',
+      tab: "Présentation de l'asso"
     },
     {
       key: 'members',
-      tab: 'Membres',
+      tab: 'Membres'
     },
     {
       key: 'contact',
-      tab: 'Contact',
-    },
-  ];
-  contentList = {};
+      tab: 'Contact'
+    }
+  ]
+  contentList = {}
 
   state = {
-    key: 'presentation',
-  };
+    key: 'presentation'
+  }
 
   addImage = image => this.setState({ image })
   render() {
-    const { login, assos, allasso } = this.props
+    const { login, assos, allasso, admin } = this.props
     const isInAsso = assos.find(a => a.login === login)
     if (!isInAsso) return <Redirect to='/' />
     const asso = { ...allasso.find(a => a.login === login), ...isInAsso }
-    console.log('asso:', asso) // @fred toutes les informations d'une asso se trouvent dans la variable asso ici
-    
-    const president = asso.members.find(function(element) {
-      return (element.group === 'Bureau' && element.role === 'president')
-    });
-    var groupsAsso = {};
-    for (let member of asso.members){
-      if (groupsAsso[member.group] === undefined){
-        groupsAsso[member.group] = [];
+
+    const president = (asso.members || []).find(element => {
+      return element.group === 'Bureau' && element.role === 'president'
+    })
+    var groupsAsso = {}
+    if (!asso.members) asso.members = []
+    asso.members.forEach(member => {
+      if (groupsAsso[member.group] === undefined) {
+        groupsAsso[member.group] = []
       }
-      groupsAsso[member.group].push(member);
-    }
+      groupsAsso[member.group].push(member)
+    })
     var membersUl = []
-    const style1 = {'listStyleType': "none"}
-    for (let group in groupsAsso){
+    const style1 = { listStyleType: 'none' }
+    for (let group in groupsAsso) {
       var sublist = []
-      for (let member of groupsAsso[group]){
-        let profileUrl = "https://etu.utt.fr/user/"+member.login;
+      for (let member of groupsAsso[group]) {
         sublist.push(
-          <li style ={style1}><a href={profileUrl}>{member.fullName}</a></li>
+          <li style={style1} key={member.login}>
+            <a href={'https://etu.utt.fr/user/' + member.login}>
+              {member.fullName} ({member.role}){/* TODO translation */}
+            </a>
+          </li>
         )
       }
-      membersUl.push(<li style ={style1}><b>{group} :</b><ul>{sublist}</ul><br /></li>)
+      membersUl.push(
+        <li style={style1} key={group}>
+          <b>{group} :</b>
+          <ul>{sublist}</ul>
+          <br />
+        </li>
+      )
     }
     var compositionAsso = <ul>{membersUl}</ul>
-    
+
     this.contentList = {
       presentation: asso.descriptionShort,
-      contact:  <p><b>Président : </b>{president.fullName}<br />
-                <b>Télephone de contact : </b>{asso.phone}<br />
-                <b>Mail de contact : </b><a>{asso.mail}</a><br />
-                <b>Site internet : </b><a>{asso.website}</a></p>,
-      members: compositionAsso,
-    };
+      contact: (
+        <p>
+          <b>Président : </b>
+          {president ? president.fullName : '...'}
+          <br />
+          <b>Télephone de contact : </b>
+          {asso.phone}
+          <br />
+          <b>Mail de contact : </b>
+          <a>{asso.mail}</a>
+          <br />
+          <b>Site internet : </b>
+          <a>{asso.website}</a>
+        </p>
+      ),
+      members: compositionAsso
+    }
 
     if (!asso) return <Spin />
     return (
@@ -86,11 +105,11 @@ class AssoManagement extends React.Component {
           tabList={this.tabList}
           activeTabKey={this.state.key}
           onTabChange={key => {
-            console.log("selected:", key);
-            this.setState({['key'] : key});
+            console.log('selected:', key)
+            this.setState({ ['key']: key })
           }}
         >
-        {this.contentList[this.state.key]}
+          {this.contentList[this.state.key]}
         </Card>
         <br />
         <DiapoImage asso={asso} />
@@ -102,7 +121,8 @@ class AssoManagement extends React.Component {
 const mapStateToProps = state => ({
   assos: state.user.assos,
   allasso: state.asso.assos,
-  login: state.routing.location.pathname.split('/assos/')[1]
+  login: state.routing.location.pathname.split('/assos/')[1],
+  admin: state.user.user.admin
 })
 
 const mapDispatchToProps = dispatch => ({})
