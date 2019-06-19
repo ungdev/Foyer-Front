6c164d3409f850f8c448f680ecad3f93350cd904
Arnaud9145 <arnaud.dufour@utt.fr>
2019-06-19 10:42:45 +0200

    changed select display for perms

--- src/containers/dashboard/pages/components/PermModal.jsx
+++ src/containers/dashboard/pages/components/PermModal.jsx
@@ -4,14 +4,51 @@ import { Modal, Form, Select, Spin } from 'antd'
 const { Option } = Select
 
 class PermModal extends React.Component {
+  getDayId = day => {
+    switch (day) {
+      case 'Lundi':
+        return 1
+      case 'Mardi':
+        return 2
+      case 'Mercredi':
+        return 3
+      case 'Jeudi':
+        return 4
+      case 'Vendredi':
+        return 5
+      case 'Samedi':
+        return 6
+      case 'Dimanche':
+        return 7
+      default:
+        return 0
+    }
+  }
   render() {
     const { visible, onCancel, onCreate, form, affichage } = this.props
-    let perms = this.props.perms.filter(perm => {
-      const found = ((affichage && affichage.perms) || []).find(
-        p => p.id === perm.id
-      )
-      return !found
-    })
+    let perms = this.props.perms
+      .filter(perm => {
+        const found = ((affichage && affichage.perms) || []).find(
+          p => p.id === perm.id
+        )
+        return !found
+      })
+      .sort((a, b) => {
+        if (this.getDayId(a.day) > this.getDayId(b.day)) return 1
+        if (this.getDayId(a.day) < this.getDayId(b.day)) return -1
+        if (
+          parseInt(a.start.split('h')[0], 10) >
+          parseInt(b.start.split('h')[0], 10)
+        )
+          return 1
+        if (
+          parseInt(a.start.split('h')[0], 10) <
+          parseInt(b.start.split('h')[0], 10)
+        )
+          return -1
+        return 0
+      })
+    console.log(perms)
     const { getFieldDecorator } = form
     return (
       <Modal
@@ -42,6 +79,13 @@ class PermModal extends React.Component {
                   {perms.map(perm => (
                     <Option key={perm.id} value={perm.id}>
                       {`${perm.day} ${perm.start}/${perm.end}`}
+                      {perm.name
+                        ? perm.name
+                        : perm.orgas && perm.orgas.length > 0
+                        ? ' (' +
+                          perm.orgas.map(orga => orga.name).join(', ') +
+                          ')'
+                        : ''}
                     </Option>
                   ))}
                 </Select>
